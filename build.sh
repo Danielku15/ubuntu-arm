9bf16371ef26a7691a2b594f1ba79845591ced09
Danielku15 <danielku15@coderline.net>
2025-04-21 14:42:07 +0200

    build: squash commands

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
-apt-get update
-apt-get install -y aptitude
-apt-get install -y build-essential
 dpkg --add-architecture arm64
 mv ./ubuntu.sources /etc/apt/sources.list.d/ubuntu.sources
 apt-get update
-aptitude install -y crossbuild-essential-arm64 libstdc++-11-dev-arm64-cross
-aptitude install -y libfontconfig-dev:arm64 libgl1-mesa-dev:arm64 libglu1-mesa-dev:arm64 freeglut3-dev:arm64
+apt-get install -y build-essential crossbuild-essential-arm64 libstdc++-11-dev-arm64-cross libfontconfig-dev:arm64 libgl1-mesa-dev:arm64 libglu1-mesa-dev:arm64 freeglut3-dev:arm64
 
+echo /usr/aarch64-linux-gnu/include
 ls -l /usr/aarch64-linux-gnu/include/
+
+echo /usr/aarch64-linux-gnu/include/c++/
 ls -l /usr/aarch64-linux-gnu/include/c++/
+
+echo /usr/include/aarch64-linux-gnu/
 ls -l /usr/include/aarch64-linux-gnu/
+
+echo /usr/include
 ls -l /usr/include
+
+echo CMake Version
 cmake --version
